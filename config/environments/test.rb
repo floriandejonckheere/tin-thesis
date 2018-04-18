913a9b28e825f2e71aba4c0f21a720e559443142
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-04-18 22:38:31 +0200

    [app] Disable ActionController and ActiveJob

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -22,16 +22,16 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  # config.action_controller.perform_caching = false
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
   # Disable request forgery protection in test environment.
-  config.action_controller.allow_forgery_protection = false
+  # config.action_controller.allow_forgery_protection = false
 
   # Store uploaded files on the local file system in a temporary directory
-  config.active_storage.service = :test
+  # config.active_storage.service = :test
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
