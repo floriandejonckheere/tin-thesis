afe031c0ef1e7be6e4e457b84a3d89080c933b14
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-05-13 11:08:23 +0200

    [app] Remove MongoDB::Event factory

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,11 +38,10 @@ puts "Creating #{FACTOR} :created events"
 FACTOR.times do
   topic = topics.sample
 
-  FactoryBot.create :mongodb_event,
-                    :predicate => :created,
-                    :subject => subjects.sample,
-                    :object => topic,
-                    :topic => topic
+  MongoDB::Event.create! :predicate => :created,
+                         :subject => subjects.sample,
+                         :object => topic,
+                         :topic => topic
 end
 
 ## Updates on topics
@@ -50,31 +49,28 @@ puts "Creating #{FACTOR * 3} :updated and :renamed events"
 (FACTOR * 3).times do
   topic = topics.sample
 
-  FactoryBot.create :mongodb_event,
-                    :predicate => weighted_rand(:updated => 0.9, :renamed => 0.1),
-                    :subject => subjects.sample,
-                    :object => topic,
-                    :topic => topic
+  MongoDB::Event.create! :predicate => weighted_rand(:updated => 0.9, :renamed => 0.1),
+                         :subject => subjects.sample,
+                         :object => topic,
+                         :topic => topic
 end
 
 ## Annotations
 puts "Creating #{FACTOR * 5} :annotated events"
 (FACTOR * 5).times do
-  FactoryBot.create :mongodb_event,
-                    :predicate => :annotated,
-                    :subject => subjects.sample,
-                    :object => annotations.sample,
-                    :topic => topics.sample
+  MongoDB::Event.create! :predicate => :annotated,
+                         :subject => subjects.sample,
+                         :object => annotations.sample,
+                         :topic => topics.sample
 end
 
 ## Comments
 puts "Creating #{FACTOR * 4} :commented events"
 (FACTOR * 4).times do
-  FactoryBot.create :mongodb_event,
-                    :predicate => :commented,
-                    :subject => subjects.sample,
-                    :object => comments.sample,
-                    :topic => topics.sample
+  MongoDB::Event.create! :predicate => :commented,
+                         :subject => subjects.sample,
+                         :object => comments.sample,
+                         :topic => topics.sample
 end
 
 ## Reactions
@@ -82,9 +78,8 @@ puts "Creating #{FACTOR} :reacted events"
 (FACTOR / 5).times do
   topic = topics.sample
 
-  FactoryBot.create :mongodb_event,
-                    :predicate => :reacted,
-                    :subject => subjects.sample,
-                    :object => topic,
-                    :topic => topic
+  MongoDB::Event.create! :predicate => :reacted,
+                         :subject => subjects.sample,
+                         :object => topic,
+                         :topic => topic
 end
--- spec/factories/mongo_db/event_factory.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :mongodb_event, :class => MongoDB::Event do
-    predicate { %i[created updated renamed commented annotated reacted].sample }
-  end
-end
