c0b17b8e0ae7301f435e33dffc85fcee7e5ca728
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-05-16 21:27:19 +0200

    [app] Format benchmark output

--- thesis/app/spec/support/benchmark.rb
+++ thesis/app/spec/support/benchmark.rb
@@ -2,40 +2,31 @@ require 'benchmark'
 
 # Number of iterations to perform
 ITERATIONS = [
-    1_000,
-    10_000,
-# 100_000,
-# 1_000_000,
+  1_000,
+  # 10_000,
+  # 100_000,
 ].freeze
 
 # Count of queried events
 COUNT = [
-    100,
-# 1_000,
-# 10_000,
+  100,
+  # 1_000,
+  # 10_000,
 ].freeze
 
 ##
 # Execute block benchmark according to parameters
 #
 def benchmark(&block)
-  results = {}
-
   ITERATIONS.each do |iteration|
-    results[iteration] = {}
-
     COUNT.each do |count|
-      b = Benchmark.measure do
-        iteration.times { block.call }
+      it "executes #{iteration} iterations with limit #{count}" do
+        b = Benchmark.measure do
+          iteration.times { block.call }
+        end
+
+        puts 'it = %8s, co = %4s, ti = %3ss' % [iteration, count, b.real.round(2)]
       end
-
-      results[iteration][count] = b.real
-    end
-  end
-
-  results.each do |iterations, count_h|
-    count_h.each do |count, time|
-      puts "it = #{iterations}, co = #{count}, ti = #{time}"
     end
   end
 end
