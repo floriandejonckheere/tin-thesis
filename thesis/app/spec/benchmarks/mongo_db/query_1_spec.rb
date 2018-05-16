5b9042902096eebe3a517ccc9f8ae3ea55c75ffc
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-05-16 21:28:23 +0200

    [app] Correct indentation

--- thesis/app/spec/benchmarks/mongo_db/query_1_spec.rb
+++ thesis/app/spec/benchmarks/mongo_db/query_1_spec.rb
@@ -8,10 +8,11 @@ RSpec.describe 'MongoDB' do
   context 'querying' do
     describe 'Select N most recent events, ordered reverse chronologically' do
       benchmark do
-        MongoDB::Event.all
-                      .order_by(:created_at => :desc)
-                      .limit(100)
-                      .each(&:to_s)
+        MongoDB::Event
+          .all
+          .order_by(:created_at => :desc)
+          .limit(100)
+          .each(&:to_s)
       end
     end
   end
