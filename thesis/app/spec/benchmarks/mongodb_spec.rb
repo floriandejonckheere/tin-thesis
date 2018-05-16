cccd06556d87d71f3c433e3a3db8fc1640388bd5
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-05-16 19:40:53 +0200

    [app] Add MongoDB spec descriptions

---
+++ thesis/app/spec/benchmarks/mongodb_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+RSpec.describe 'MongoDB' do
+  context 'querying' do
+    describe 'Select N most recent events, ordered reverse chronologically'
+
+    describe 'Select N most recent events, where the event is related to a topic in a list of given topics, ordered reverse chronologically'
+
+    describe 'Select N most recent events, where the event is related to a given topic, ordered reverse chronologically'
+
+    describe 'Select N most recent events, where the event is related to a given user, ordered reverse chronologically'
+  end
+
+  context 'inserting' do
+    describe 'Insert N given events'
+  end
+end
