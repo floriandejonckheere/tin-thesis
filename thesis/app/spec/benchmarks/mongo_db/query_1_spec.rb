f84bd7c5f5fa667e1bd82a4e2014f9a02b9aa3fa
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-05-16 21:18:01 +0200

    [app] Add MongoDB query 1

---
+++ thesis/app/spec/benchmarks/mongo_db/query_1_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'benchmark'
+
+require 'rails_helper'
+
+RSpec.describe 'MongoDB' do
+  context 'querying' do
+    describe 'Select N most recent events, ordered reverse chronologically' do
+      benchmark do
+        MongoDB::Event.all
+                      .order_by(:created_at => :desc)
+                      .limit(100)
+                      .each(&:to_s)
+      end
+    end
+  end
+end
