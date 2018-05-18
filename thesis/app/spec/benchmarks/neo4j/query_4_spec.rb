997a41eb34640d6c23a05f5d59ab2b6882a9e655
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-05-18 16:13:43 +0200

    [app] Add Neo4j query 4

---
+++ thesis/app/spec/benchmarks/neo4j/query_4_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'benchmark'
+
+require 'rails_helper'
+
+RSpec.describe 'Neo4j' do
+  context 'querying' do
+    describe 'Select N most recent events, where the event is related to a given user, ordered reverse chronologically' do
+      # Select random user
+      id = Neo4j::Subject.skip(rand Neo4j::Subject.count).first.id
+
+      benchmark do |count|
+        Neo4j::Subject
+            .find(id)
+            .events
+            .order(:created_at => :desc)
+            .limit(count)
+            .each(&:to_s)
+      end
+    end
+  end
+end
