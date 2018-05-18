ede4c70b202b67bda79d85ea49c57b67e1907a98
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-05-18 21:05:21 +0200

    [app] Add MongoDB comment topic

--- thesis/app/app/models/mongo_db/comment.rb
+++ thesis/app/app/models/mongo_db/comment.rb
@@ -11,17 +11,23 @@ module MongoDB
     embeds_one :subject,
                :class_name => 'MongoDB::Subject'
 
+    embeds_one :topic,
+               :class_name => 'MongoDB::Topic'
+
     ##
     # Validations
     #
     validates :subject,
               :presence => true
 
+    validates :topic,
+              :presence => true
+
     ##
     # Methods
     #
     def to_s
-      'comment'
+      "#{subject}'s comment on #{topic}"
     end
   end
 end
