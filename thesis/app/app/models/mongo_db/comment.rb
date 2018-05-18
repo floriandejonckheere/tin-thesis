655a7502e4f22c881183c5ed44ad4a31f6c2d41d
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-05-18 21:06:28 +0200

    [app] Demodulize MongoDB::Item inheritance

--- thesis/app/app/models/mongo_db/comment.rb
+++ thesis/app/app/models/mongo_db/comment.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module MongoDB
-  class Comment < MongoDB::Item
+  class Comment < Item
     ##
     # Properties
     #
--- thesis/app/app/models/mongo_db/topic.rb
+++ thesis/app/app/models/mongo_db/topic.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module MongoDB
-  class Topic < MongoDB::Item
+  class Topic < Item
     ##
     # Properties
     #
