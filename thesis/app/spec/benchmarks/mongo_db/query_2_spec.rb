e5d97ae1e2ea620e79f17a85ad05ba31083ea54c
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-05-18 21:35:24 +0200

    [app] Rewrite MongoDB query 2

--- thesis/app/spec/benchmarks/mongo_db/query_2_spec.rb
+++ thesis/app/spec/benchmarks/mongo_db/query_2_spec.rb
@@ -6,6 +6,21 @@ require 'rails_helper'
 
 RSpec.describe 'MongoDB' do
   context 'querying' do
-    describe 'Select N most recent events, where the event is related to a topic in a list of given topics, ordered reverse chronologically'
+    describe 'Select N most recent events, where the event is related to a topic in a list of given topics, ordered reverse chronologically' do
+      # Select 50 random topics
+      ids = MongoDB::Event
+            .where('item._type' => 'MongoDB::Topic')
+            .skip(rand MongoDB::Event.where('item._type' => 'MongoDB::Topic').count)
+            .limit(50)
+            .map { |e| e.item.id }
+
+      benchmark do |count|
+        MongoDB::Event
+          .in('item._id' => ids)
+          .order_by(:created_at => :desc)
+          .limit(count)
+          .each(&:to_s)
+      end
+    end
   end
 end
