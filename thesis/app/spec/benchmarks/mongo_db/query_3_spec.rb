4411b6866b80f035c5c4c7f901d00cf3c1305806
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-05-18 21:37:33 +0200

    [app] Add MongoDB query 3

--- thesis/app/spec/benchmarks/mongo_db/query_3_spec.rb
+++ thesis/app/spec/benchmarks/mongo_db/query_3_spec.rb
@@ -6,6 +6,22 @@ require 'rails_helper'
 
 RSpec.describe 'MongoDB' do
   context 'querying' do
-    describe 'Select N most recent events, where the event is related to a given topic, ordered reverse chronologically'
+    describe 'Select N most recent events, where the event is related to a given topic, ordered reverse chronologically' do
+      # Select random topic
+      id = MongoDB::Event
+           .where('item._type' => 'MongoDB::Topic')
+           .skip(rand MongoDB::Event.where('item._type' => 'MongoDB::Topic').count)
+           .first
+           .item
+           .id
+
+      benchmark do |count|
+        MongoDB::Event
+          .where('item._id' => id)
+          .order_by(:created_at => :desc)
+          .limit(count)
+          .each(&:to_s)
+      end
+    end
   end
 end
