e0bb994fcda348fc5ebebbc9759201915a0a198f
Florian Dejonckheere <florian@floriandejonckheere.be>
2018-05-19 07:39:43 +0200

    [app] Add MongoDB query 5

--- thesis/app/spec/benchmarks/mongo_db/query_5_spec.rb
+++ thesis/app/spec/benchmarks/mongo_db/query_5_spec.rb
@@ -6,6 +6,27 @@ require 'rails_helper'
 
 RSpec.describe 'MongoDB' do
   context 'insertion' do
-    describe 'Insert N given events'
+    describe 'Insert N given events' do
+      # Select random user
+      subject_id = MongoDB::Event
+                   .skip(rand MongoDB::Event.count)
+                   .first
+                   .subject
+                   .id
+
+      # Select random topic
+      item_id = MongoDB::Event
+                .where('item._type' => 'MongoDB::Topic')
+                .skip(rand MongoDB::Event.where('item._type' => 'MongoDB::Topic').count)
+                .first
+                .item
+                .id
+
+      benchmark do |count|
+        MongoDB::Event.create! :subject => subject_id,
+                               :item => item_id,
+                               :predicate => :updated
+      end
+    end
   end
 end
